b79b66f63fee3f348a692b618cdcc413251b3d8a
Vladislav <nowspamhere@gmail.com>
2016-09-27 01:44:21 +0300

    Configure datasources

--- src/main/java/com/epam/cdp/jee/util/Resources.java
+++ src/main/java/com/epam/cdp/jee/util/Resources.java
@@ -4,6 +4,8 @@ import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.logging.Logger;
 
 /**
@@ -20,6 +22,9 @@ import java.util.logging.Logger;
  */
 public class Resources {
 
+    @Produces
+    @PersistenceContext
+    private EntityManager em;
 
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
